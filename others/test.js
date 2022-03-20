9cf2b70a8ce19463a440f87e13f5d05405fc4745
Dinh Phuc <phuc1603>
2022-03-20 10:51:28 +0700

    Add source

--- others/test.js
+++
@@ -1,6 +0,0 @@
-var { PythonShell } = require('python-shell');
-
-let shell = new PythonShell('funcTest.py', { mode: 'text' });
-shell.on('message', function (message) {
-    console.log(JSON.parse(message.replace(/'/g, '"')));
-});
